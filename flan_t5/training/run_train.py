cb3713608d6b97b0b7f9a528a399d5f308a37080
Kevin Chung <k.chung360@gmail.com>
2023-10-11 15:04:36 -0700

    Update run_train.py

--- flan_t5/training/run_train.py
+++ flan_t5/training/run_train.py
@@ -135,20 +135,20 @@ def training_function(args):
         # device_map="auto"
     )
     # Define LoRA Config
-    lora_config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        target_modules=["q", "v"],
-        lora_dropout=0.05,
-        bias="none",
-        task_type=TaskType.SEQ_2_SEQ_LM
-    )
-    # prepare int-8 model for training
-    model = prepare_model_for_int8_training(model)
+    # lora_config = LoraConfig(
+    #     r=16,
+    #     lora_alpha=32,
+    #     target_modules=["q", "v"],
+    #     lora_dropout=0.05,
+    #     bias="none",
+    #     task_type=TaskType.SEQ_2_SEQ_LM
+    # )
+    # # prepare int-8 model for training
+    # model = prepare_model_for_int8_training(model)
 
-    # add LoRA adaptor
-    model = get_peft_model(model, lora_config)
-    model.print_trainable_parameters()
+    # # add LoRA adaptor
+    # model = get_peft_model(model, lora_config)
+    # model.print_trainable_parameters()
 
     # max_memory = get_balanced_memory(
     #     model,
